44f0a254ac02ea1e914c80f77a048f6a1c08f28d
monsur28 <monsur286512@gmail.com>
2024-09-20 02:38:38 +0600

    Components Structure Updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,24 @@
-
 import Brand from "@/components/Brand";
-
-
 import Community from "@/components/Community";
 import Analytics from "@/components/Analytics";
 import Blogs from "@/components/Blogs";
-
 import Footer from "@/components/Footer";
 import Review from "@/components/Review";
 import Navbar from "@/components/Navbar";
-<<<<<<< HEAD
 import Banner from "@/components/Banner";
-=======
 import PopularBooks from "@/components/PopularBooks";
-
->>>>>>> 4259562576e9a9d3661028dc7cc41b62763b4136
 
 export default function Home() {
   return (
     <>
-    <Navbar></Navbar>
-    <Banner></Banner>
-    <Community></Community>
-      <Review></Review>
+      <Navbar />
+      <Banner />
+      <PopularBooks />
+      <Brand />
+      <Review />
       <Blogs />
-      <Community></Community>
+      <Community />
       <Analytics />
-      <PopularBooks></PopularBooks>
-      <Brand></Brand>
       <Footer />
     </>
   );
