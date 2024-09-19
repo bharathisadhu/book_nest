4ab39742b20929540fab520d63f5e5da9d1c02c4
Badhon-Sarker <badhonsarker38@gmail.com>
2024-09-20 00:10:40 +0600

    review part added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Footer from "@/components/Footer";
+import Review from "@/components/Review";
 
 
 export default function Home() {
   return (
     <>
-   
+      <Review></Review>
       <Footer />
     </>
   );
