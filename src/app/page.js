aac71ff457f37fd91d5d3f63ebbf713ff84dc74f
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-09-20 02:28:26 +0600

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,13 @@
 
 import Brand from "@/components/Brand";
-
-
 import Community from "@/components/Community";
 import Analytics from "@/components/Analytics";
 import Blogs from "@/components/Blogs";
-
 import Footer from "@/components/Footer";
 import Review from "@/components/Review";
 import Navbar from "@/components/Navbar";
-<<<<<<< HEAD
 import Banner from "@/components/Banner";
-=======
 import PopularBooks from "@/components/PopularBooks";
-
->>>>>>> 4259562576e9a9d3661028dc7cc41b62763b4136
 
 export default function Home() {
   return (
