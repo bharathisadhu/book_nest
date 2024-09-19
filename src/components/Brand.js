f4769db750d4951c2e2f82c556c5e5de52c08d0a
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-09-20 01:53:26 +0600

    addeded brand section

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,17 +1,17 @@
+"use client";
 // Import Swiper React components
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
-
+import "swiper/css/navigation";
 // import required modules
 import { Autoplay, Navigation } from "swiper/modules";
 import Image from "next/image";
 
-export default function Brand(){
-    return (
-        <div className="container mx-auto my-6 md:my-14 lg:my-28">
+export default function Brand() {
+  return (
+    <div className="container mx-auto my-6 md:my-14 lg:my-28">
       <Swiper
         slidesPerView={3} // Default for large screens (desktop)
         spaceBetween={30}
@@ -44,8 +44,8 @@ export default function Brand(){
       >
         <SwiperSlide>
           <Image
-          width={500}
-          height={500}
+            width={500}
+            height={500}
             className="md:h-20 lg:h-32"
             src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_1.svg"
             alt="Brand 1"
@@ -53,8 +53,8 @@ export default function Brand(){
         </SwiperSlide>
         <SwiperSlide>
           <Image
-          width={500}
-          height={500}
+            width={500}
+            height={500}
             className="md:h-20 lg:h-32"
             src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_2.svg"
             alt="Brand 2"
@@ -62,8 +62,8 @@ export default function Brand(){
         </SwiperSlide>
         <SwiperSlide>
           <Image
-          width={500}
-          height={500}
+            width={500}
+            height={500}
             className="md:h-20 lg:h-32"
             src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_3.svg"
             alt="Brand 3"
@@ -71,8 +71,8 @@ export default function Brand(){
         </SwiperSlide>
         <SwiperSlide>
           <Image
-          width={500}
-          height={500}
+            width={500}
+            height={500}
             className="md:h-20 lg:h-32"
             src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_4.svg"
             alt="Brand 4"
@@ -80,8 +80,8 @@ export default function Brand(){
         </SwiperSlide>
         <SwiperSlide>
           <Image
-          width={500}
-          height={500}
+            width={500}
+            height={500}
             className="md:h-20 lg:h-32"
             src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_5.svg"
             alt="Brand 5"
@@ -89,8 +89,8 @@ export default function Brand(){
         </SwiperSlide>
         <SwiperSlide>
           <Image
-          width={500}
-          height={500}
+            width={500}
+            height={500}
             className="md:h-20 lg:h-32"
             src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_6.svg"
             alt="Brand 6"
@@ -98,8 +98,8 @@ export default function Brand(){
         </SwiperSlide>
         <SwiperSlide>
           <Image
-          width={500}
-          height={500}
+            width={500}
+            height={500}
             className="h-16 md:h-20 lg:h-36"
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4kWGkqPFdXNC5JisMO8VUSusMafaFfKFnQA&s"
             alt="Brand 7"
@@ -107,8 +107,8 @@ export default function Brand(){
         </SwiperSlide>
         <SwiperSlide>
           <Image
-          width={500}
-          height={500}
+            width={500}
+            height={500}
             className="h-20 lg:h-40"
             src="https://cdn.freebiesupply.com/logos/large/2x/the-black-book-logo-png-transparent.png"
             alt="Brand 8"
@@ -116,8 +116,8 @@ export default function Brand(){
         </SwiperSlide>
         <SwiperSlide>
           <Image
-          width={500}
-          height={500}
+            width={500}
+            height={500}
             className="h-20 lg:h-40"
             src="https://d3bzyjrsc4233l.cloudfront.net/news/Peach_sSVKDnq.png"
             alt="Brand 9"
@@ -125,5 +125,5 @@ export default function Brand(){
         </SwiperSlide>
       </Swiper>
     </div>
-    )
+  );
 }
