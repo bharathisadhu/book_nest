1652bcee1c230d29e5878ad9e45cda0eada845bf
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-09-20 01:21:15 +0600

    NavBar

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,12 @@ import Analytics from "@/components/Analytics";
 import Blogs from "@/components/Blogs";
 import Footer from "@/components/Footer";
 import Review from "@/components/Review";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
-
+    <Navbar></Navbar>
     <Community></Community>
       <Review></Review>
       <Blogs />
