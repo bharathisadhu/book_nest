b4676769fa0d95b336d5d4ee2ca2b1edfe262033
jrazad10 <jrazad10@gmail.com>
2024-09-20 09:54:12 +0600

    fixed few issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,52 @@
 
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {};
+
+// // export default nextConfig;
+
+// // next.config.js
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+// const nextConfig = {
+//   images: {
+//     domains: [
+//       "static1.srcdn.com",
+//       "s.yimg.com",
+//       "discoverscifi.com",
+//       "wp.penguin.co.uk",
+//       "miro.medium.com",
+//       "cdn.shopify.com",
+//       "blueroseone.com",
+//       "img1.rapidleaks.com",
+//       "assets.penguinrandomhouse.com",
+//       "thewritepractice.com",
+//       "demo2.pavothemes.com",
+//       "i.ibb.co.com",
+//       "demo2.pavothemes.com",
+//       "encrypted-tbn0.gstatic.com",
+//       "cdn.freebiesupply.com",
+//       "d3bzyjrsc4233l.cloudfront.net",
+//       "demo2.pavothemes.com",
+//     ],
+//   },
+// };
 
-// export default nextConfig;
 
-// next.config.js
-/** @type {import('next').NextConfig} */
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//       domains: ['i.ibb.co'],
+//     },
+//   };
+  
+//   export default nextConfig;
+  
+  
+
+
+
+
+  /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: [
@@ -20,25 +61,13 @@ const nextConfig = {
       "assets.penguinrandomhouse.com",
       "thewritepractice.com",
       "demo2.pavothemes.com",
-      "i.ibb.co.com",
-      "demo2.pavothemes.com",
+      "i.ibb.co",
       "encrypted-tbn0.gstatic.com",
       "cdn.freebiesupply.com",
       "d3bzyjrsc4233l.cloudfront.net",
-      "demo2.pavothemes.com",
+      'i.ibb.co.com'
     ],
   },
 };
 
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['i.ibb.co'],
-    },
-  };
-  
-  export default nextConfig;
-  
-  
+export default nextConfig;
