38e8a9a5f9cf45a39e7172bad254451bf27c7c82
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-09-24 03:48:16 +0600

    added authentication and polish ui

---
+++ src/app/register/api/route.js
@@ -0,0 +1,27 @@
+import connectDB from "@/lib/connectDB";
+import bcrypt from "bcrypt";
+export const POST = async (request) => {
+  const newUser = await request.json();
+  try {
+    const db = await connectDB();
+    const userCollection = db.collection("users");
+    const exist = await userCollection.findOne({ email: newUser.email });
+    if (exist) {
+      return Response.json({ message: "user Exists" }, { status: 304 });
+    }
+    const hashedPassword = bcrypt.hashSync(newUser.password, 14);
+    const res = await userCollection.insertOne({
+      ...newUser,
+      password: hashedPassword,
+    });
+    return Response.json(
+      { message: "new User created", data: res },
+      { status: 200 }
+    );
+  } catch (error) {
+    return Response.json(
+      { message: "something went wrong", error },
+      { status: 500 }
+    );
+  }
+};
