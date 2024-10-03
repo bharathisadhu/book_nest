11d40276808ffa7e09bce01fc81d7c2bc59808cd
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-10-03 12:51:52 +0600

    polishing books page ui

--- src/components/BooksCard.js
+++ src/components/BooksCard.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Image from "next/image";
 import { CiStar } from "react-icons/ci";
 import Swal from "sweetalert2";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaHeart, FaShoppingCart } from "react-icons/fa";
 
 export default function BooksCard({ book }) {
   const { name, image, price, category, ratings, _id, quantity } = book;
@@ -115,7 +115,7 @@ export default function BooksCard({ book }) {
   };
 
   return (
-    <div className="transition-shadow h-fit duration-300 w-full font-sans overflow-hidden mx-auto mt-4 pl-4 pt-4 rounded-lg shadow-lg border border-gray-200">
+    <div className="transition h-fit duration-500 w-full font-sans overflow-hidden mx-auto mt-4 pl-4 pt-4 hover:scale-105">
       {/* Full Height Image */}
       <div className="w-full h-48 relative">
         <Image
@@ -123,10 +123,10 @@ export default function BooksCard({ book }) {
           alt={name}
           layout="fill"
           objectFit="cover"
-          className="rounded-t-lg"
+          className="rounded-t-lg hover:scale-105 transition duration-500"
         />
         {/* Bookmark Icon on the left side */}
-        <button
+        {/* <button
           onClick={addToBookmark}
           className={`absolute top-2 right-2 p-2 rounded-full bg-white shadow-md transition-colors duration-200 ${
             isBookmarked ? "text-yellow-500" : "text-gray-600"
@@ -140,7 +140,7 @@ export default function BooksCard({ book }) {
           >
             <path d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"></path>
           </svg>
-        </button>
+        </button> */}
       </div>
 
       {/* Book Details */}
@@ -161,31 +161,21 @@ export default function BooksCard({ book }) {
           </p>
         </div>
 
-        <h3 className="text-xl md:text-2xl text-gray-800 font-bold">
+        <h3 className="text-xl md:text-2xl text-gray-800 font-bold my-2">
           ${price.toFixed(2)}
         </h3>
         <div className="flex items-center justify-between">
-          <div className="bg-pink-100 w-10 h-10 md:w-12 md:h-12 flex items-center justify-center rounded-full cursor-pointer hover:bg-pink-200 transition duration-200">
+          <div className="flex items-center justify-center rounded-full cursor-pointer hover:scale-125 transition duration-200">
             <button onClick={addToBookmark}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20px"
-                className="fill-pink-600"
-                viewBox="0 0 64 64"
-              >
-                <path d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"></path>
-              </svg>
+              <FaHeart className="text-3xl" />
             </button>
           </div>
-          <div className="bg-pink-100 w-10 h-10 md:w-12 md:h-12 flex items-center justify-center rounded-full cursor-pointer hover:bg-pink-200 transition duration-200">
+          <div className=" flex items-center justify-center rounded-full cursor-pointer hover:scale-125 transition duration-200 mt-2">
             <button onClick={addToCart}>
-              <FaShoppingCart />
+              <FaShoppingCart className="text-3xl" />
             </button>
           </div>
         </div>
-        <h3 className="text-xl md:text-2xl text-gray-800 font-bold mt-4">
-          ${price.toFixed(2)}
-        </h3>
       </div>
     </div>
   );
