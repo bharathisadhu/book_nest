de948f3d9e7d6a0dfbcc827386fd21cdb19badc5
jrazad10 <jrazad10@gmail.com>
2024-10-08 17:31:59 +0600

    delete option added with sweet alert in users list in admin dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,17 +1,17 @@
 // src/app/dashboard/users/page.js
 import DashboardLayout from "@/components/DashboardLayout";
-// import RootLayout from "@/app/layout";
+import RootLayout from "@/app/layout";
 
 const Users = () => {
   return (
    
-    // <RootLayout showNavbar={false} showFooter={false}>
+    <RootLayout showNavbar={false} showFooter={false}>
       <DashboardLayout>
         <h1 className="text-2xl font-bold">This is the main dashboard</h1>
         <p>This is the dashboard page content.</p>
         {/* Add more specific content here */}
       </DashboardLayout>
-    // </RootLayout>
+    </RootLayout>
   );
 };
 
