4fce7a24119d8810c272ade0bfbbd5edd9cab867
jrazad10 <jrazad10@gmail.com>
2024-10-09 12:22:10 +0600

    solved some vercel errors after deploying users in admin dashboard

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -1,26 +1,3 @@
-// import connectToDatabase from "@/lib/mongodb";
-// import Users from "../../../../../models/Users";
-// import { NextResponse } from "next/server";
-
-// export async function PUT(request, { params }) {
-//   const  {id}  = params;
-//   const {
-//     newName: name,
-//     newEmail: email,
-//     newPassword: password,
-//     newImage: image,
-//   } = await request.json();
-//   await connectToDatabase();
-//   await Users.findByIdAndUpdate(id,  name, email, password, image );
-//   return NextResponse.json({ message: "User Updated" }, { status: 200 });
-// }
-
-// export async function GET(request, { params }) {
-//   const { id } = params;
-//   await connectToDatabase()
-//   const users = await Users.findOne({_id: id})
-//   return NextResponse.json(users, {status: 200})
-// }
 
 
 import connectToDatabase from "@/lib/mongodb";
--- src/app/dashboard/editUsers/[id]/page.js
+++ src/app/dashboard/editUsers/[id]/page.js
@@ -1,44 +1,4 @@
-// import EditUserForm from "@/components/EditUserForm";
 
-// const getUsersById = async (id) => {
-//   try {
-//     const res = await fetch(
-//       `${process.env.NEXT_PUBLIC_API_URL}/api/users/${id}`,
-//       {
-//         cache: "no-store",
-//       }
-//     );
-
-//     if (!res.ok) {
-//       throw new Error("Failed to fetch user");
-//     }
-
-//     return res.json();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export default async function EditUser({ params }) {
-//   const { id } = params;
-//   const user = await getUsersById(id); // Call the function here
-
-//   if (!user) {
-//     return <div>User not found</div>;
-//   }
-
-//   const { name, email, password, image } = user; // Adjust destructuring
-
-//   return (
-//     <EditUserForm
-//       id={id}
-//       name={name}
-//       email={email}
-//       password={password}
-//       image={image}
-//     />
-//   );
-// }
 
 
 import EditUserForm from "@/components/EditUserForm";
