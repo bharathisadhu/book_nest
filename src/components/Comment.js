27f85fbe8d5972c5f0b28ead23dc90b39af99a71
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-10-09 21:15:58 +0600

    single blog comment & reply

---
+++ src/components/Comment.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+
+const Comment = ({ comment }) => {
+  const [reply, setReply] = useState('');
+  const [showReply, setShowReply] = useState(false);
+
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+  const handleReplySubmit = async (e) => {
+    e.preventDefault();
+    const replyData = { email:comment.email, username:comment.username, blogId:comment.blogId, text: reply, parentId: comment._id };
+    
+    const response = await fetch(`${baseUrl}/api/comments`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(replyData),
+    });
+    
+    const data = await response.json();
+    // console.log("testing ------",replyData);
+    setReply('');
+    if (!comment.replies) {
+          // If replies is not already defined, initialize it as an empty array
+          comment.replies = [];
+        }
+    comment.replies.push(data); // Update local replies
+   
+    
+  };
+   
+  return (
+    <div className="comment p-4 border border-gray-300 rounded mb-4">
+      <p><span className="text-orange-800 uppercase">{comment.username}-</span><span className="text-[red] font-semibold">{comment.text}</span></p>
+      {(!comment?.parentId)&&
+          <button 
+          className="mt-2 text-blue-500"
+        onClick={() => setShowReply(!showReply)}>
+        {showReply ? 'Cancel' : 'Reply'}
+        </button>
+      }
+      
+    {showReply && (
+      <form onSubmit={handleReplySubmit} className="mt-2">
+        <textarea
+          value={reply}
+          onChange={(e) => setReply(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded"
+          placeholder="Write a reply..."
+        />
+        
+        <button 
+         className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
+        type="submit">Post Reply
+        </button>
+      </form>
+    )}
+    {comment.replies && (
+      <div>
+        {comment.replies?.length>0 && comment.replies.map((reply) => (
+          <Comment key={reply._id} comment={reply} />
+        ))}
+      </div>
+    )}
+     
+    </div>
+  );
+
+  
+};
+
+export default Comment;
---
+++ src/components/CommentsList.js
@@ -0,0 +1,80 @@
+'use client'
+import { useState,useEffect } from 'react';
+import Comment from './Comment';
+import { useSession  } from "next-auth/react";
+const CommentsList = ({id}) => {
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('');
+  const { data: session, status } = useSession();
+
+  
+  
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+   // Fetch comments from the backend
+   useEffect(() => {
+    const fetchComments = async () => {
+      const response = await fetch(`${baseUrl}/api/comments?blogId=${id}`);
+      const data = await response.json();
+      
+      setComments(data);
+          };
+    fetchComments();
+  }, []);
+
+  
+
+  // Add new comment to the comments list
+  const handleCommentSubmit =async (e) => {
+    e.preventDefault();
+    const commentData = { text: newComment,email:session.user.email,username:session.user.name,blogId:id};
+    
+    const response = await fetch(`${baseUrl}/api/comments`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(commentData),
+    });
+
+    const data = await response.json();
+   
+    setComments([...comments, { ...commentData, _id: data._id }]);
+    setNewComment('');
+
+  };
+
+  console.log("check-status",status);
+
+  return (
+    <div className="comments-list p-6 max-w-xl mx-auto">
+    {/* <h1>Welcome, {session?.user?.name}</h1>
+    <h1>Welcome, {session?.user?.email}</h1> */}
+      <h1 className="text-xl font-bold mb-4">Comments</h1>
+      
+      <form onSubmit={handleCommentSubmit} className="mb-6">
+        <textarea
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded"
+          placeholder="Write your comment..."
+        />
+        <button
+          type="submit"
+          className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
+        >
+          Submit Comment
+        </button>
+      </form>
+
+      <div className="comment-section">
+      
+      {comments?.length>0 && comments.map((comment) => (
+          <Comment key={comment._id} comment={comment} />
+        ))}
+
+
+    </div>
+    </div>
+  );
+};
+
+export default CommentsList;
