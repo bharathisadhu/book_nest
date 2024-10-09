db9b4dd6a709485416fa7e5dc07d769f13899522
monsur28 <monsur286512@gmail.com>
2024-10-09 19:41:15 +0600

    polishing checkout page

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,20 +2,11 @@
 
 export default function Loader() {
   return (
-<<<<<<< HEAD
     <div className="flex items-center justify-center h-32">
       <div className="relative">
         <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
         <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin"></div>
       </div>
     </div>
-=======
-   <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-75">
-          <div className="relative">
-            <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
-            <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin"></div>
-          </div>
-        </div>
->>>>>>> 1f9dfb33fb24cefdb041c2488fdcae0623786236
   );
 }
