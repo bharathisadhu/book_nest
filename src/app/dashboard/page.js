715473a2b35b06c763380f9f5feeff265f4e93e5
Mohammad Azad <61772814+azaaaaaaaaad@users.noreply.github.com>
2024-10-10 10:40:12 +0600

    Update page.js

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,31 +1,14 @@
-// // src/app/dashboard/users/page.js
-// import DashboardLayout from "@/components/DashboardLayout";
-// // import RootLayout from "@/app/layout";
 
-// const Users = () => {
-//   return (
-//     // <RootLayout showNavbar={false} showFooter={false}>
-//     <DashboardLayout>
-//       <h1 className="text-2xl font-bold">This is the main dashboard</h1>
-//       <p>This is the dashboard page content.</p>
-//       {/* Add more specific content here */}
-//     </DashboardLayout>
-//     // </RootLayout>
-//   );
-// };
-
-// export default Users;
 
 
 import DashboardLayout from "@/components/DashboardLayout";
-// import RootLayout from "@/app/layout";
 import UserCount from "@/components/mainDashBord/UserCount";
 import BookCount from "@/components/mainDashBord/BooKCountPrice";
 import OrderCount from "@/components/mainDashBord/OrderCountPrice";
 import SaleCount from "@/components/mainDashBord/SaleCount";
 const Users = () => {
   return (
-    // <RootLayout showNavbar={false} showFooter={false}>
+
 
       <DashboardLayout>
         <div className="card bg-base-100 w-96 shadow-xl text-justify mx-10 my-5 font-sans">
@@ -50,7 +33,7 @@ const Users = () => {
       {/* Add more specific content here */}
     </DashboardLayout>
 
-    // </RootLayout>
+
   );
 };
 
