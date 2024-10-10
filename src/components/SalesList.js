2f84491a3d64d0da52e92b2420eb6643f460767d
jrazad10 <jrazad10@gmail.com>
2024-10-10 15:48:46 +0600

    all routing and updates has been done in the admin dashboard

--- src/components/SalesList.js
+++ src/components/SalesList.js
@@ -1,41 +1,39 @@
-
-
-
 const getPayments = async () => {
-    try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/payments`, {
-        cache: "no-store",
-      });
-  
-      console.log("Response Status:", res.status); // Log the response status
-  
-      if (!res.ok) {
-        throw new Error("Failed to fetch users");
-      }
-  
-      const data = await res.json();
-      console.log("API Data:", JSON.stringify(data, null, 2)); // Log the entire data structure
-      return Array.isArray(data.users) ? data.users : data || []; // Adjust according to your API response structure
-      
-    } catch (error) {
-      console.error("Error loading payments:", error.message); // Log the error message
-      return []; // Return an empty array in case of error
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/payments`, {
+      cache: "no-store",
+    });
+
+    console.log("Response Status:", res.status); // Log the response status
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch users");
     }
-  };
 
-export default async function  SalesList () {
+    const data = await res.json();
+    console.log("API Data:", JSON.stringify(data, null, 2)); // Log the entire data structure
+    return Array.isArray(data.users) ? data.users : data || []; // Adjust according to your API response structure
+  } catch (error) {
+    console.error("Error loading payments:", error.message); // Log the error message
+    return []; // Return an empty array in case of error
+  }
+};
 
-    const payments = await getPayments();
-  console.log("Payment Array Length:", payments.length); 
-  console.log("Payment List:", payments); 
+export default async function SalesList() {
+  const payments = await getPayments();
+  console.log("Payment Array Length:", payments.length);
+  console.log("Payment List:", payments);
 
   if (payments.length === 0) {
-    return <div className="text-2xl font-bold text-red-500 my-10 text-center">No users found or failed to load users.</div>; // Show a message if no users
+    return (
+      <div className="text-2xl font-bold text-red-500 my-10 text-center">
+        No users found or failed to load users.
+      </div>
+    ); // Show a message if no users
   }
 
-
-    return(
-        <div className="font-sans overflow-x-auto">
+  return (
+    <div className="font-sans overflow-x-auto">
       <table className="min-w-full divide-y divide-gray-200">
         <thead className="bg-gray-100 whitespace-nowrap">
           <tr>
@@ -59,25 +57,21 @@ export default async function  SalesList () {
         <tbody className="bg-white divide-y divide-gray-200 whitespace-nowrap">
           {Array.isArray(payments) &&
             payments.map((pay) => (
-              <tr key={pay.id}>
-                <td className="px-4 py-4 text-sm text-gray-800">
-                  {pay?.name}
-                </td>
-                <td className="px-4 py-4 text-sm text-gray-800">
-                  {pay.email}
-                </td>
+              <tr key={pay._id}>
+                <td className="px-4 py-4 text-sm text-gray-800">{pay?.name}</td>
+                <td className="px-4 py-4 text-sm text-gray-800">{pay.email}</td>
                 <td className="px-4 py-4 text-sm text-gray-800">
                   {parseFloat(pay.price).toFixed(2)} $
                 </td>
                 <td className="px-4 py-4 text-sm text-gray-800">{pay.date}</td>
 
-                <td className="px-4 py-4 text-sm text-gray-800">{pay.transactionId}</td>
-                
+                <td className="px-4 py-4 text-sm text-gray-800">
+                  {pay.transactionId}
+                </td>
               </tr>
             ))}
         </tbody>
       </table>
     </div>
-
-    )
+  );
 }
