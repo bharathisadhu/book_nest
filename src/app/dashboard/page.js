f7847ade84e02e505f1ec954d7dc1244cbda6c43
Mohammad Azad <61772814+azaaaaaaaaad@users.noreply.github.com>
2024-10-10 10:20:10 +0600

    Update page.js

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,4 +1,22 @@
-// src/app/dashboard/users/page.js
+// // src/app/dashboard/users/page.js
+// import DashboardLayout from "@/components/DashboardLayout";
+// // import RootLayout from "@/app/layout";
+
+// const Users = () => {
+//   return (
+//     // <RootLayout showNavbar={false} showFooter={false}>
+//     <DashboardLayout>
+//       <h1 className="text-2xl font-bold">This is the main dashboard</h1>
+//       <p>This is the dashboard page content.</p>
+//       {/* Add more specific content here */}
+//     </DashboardLayout>
+//     // </RootLayout>
+//   );
+// };
+
+// export default Users;
+
+
 import DashboardLayout from "@/components/DashboardLayout";
 // import RootLayout from "@/app/layout";
 
@@ -15,3 +33,4 @@ const Users = () => {
 };
 
 export default Users;
+
