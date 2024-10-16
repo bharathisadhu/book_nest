b8a4e1e62c90740d015d525ab98453d451caa834
jrazad10 <jrazad10@gmail.com>
2024-10-16 18:34:15 +0600

    ssl_payments cancel, fail, success api created

---
+++ src/app/api/success/route.js
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+export async function POST(req, res) {
+  // database operations
+  // b2b 3rd party secure call
+  // success log
+  return NextResponse.redirect(new URL("/pages/success", req.url), 303);
+}
