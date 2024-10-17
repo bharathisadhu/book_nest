f51d2bff2d3a959155bca64c98722082089dd3a2
Mohammad Azad <61772814+azaaaaaaaaad@users.noreply.github.com>
2024-10-17 12:35:37 +0600

    Update BooksCard.js

--- src/components/BooksCard.js
+++ src/components/BooksCard.js
@@ -1,4 +1,5 @@
-import { useState,useEffect } from "react";
+
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Image from "next/image";
 import Swal from "sweetalert2";
@@ -8,34 +9,24 @@ import { FaDollarSign } from "react-icons/fa6";
 import { useSession } from "next-auth/react";
 
 export default function BooksCard({ book }) {
-
- 
-  const { name, image, price, category, ratings, _id, quantity,publishType } = book;
-
-  const { name, image, price, category, ratings, _id, quantity, cardCount } =
-    book;
+  const { name, image, price, category, ratings, _id, quantity, publishType, cardCount } = book;
 
   const [isBookmarked, setIsBookmarked] = useState(false);
-  const [isInCart, setIsInCart] = useState(false)
+  const [isInCart, setIsInCart] = useState(false);
   const { data: session } = useSession();
 
   const [stock, setStock] = useState(null);
   const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
-
-useEffect(() => {
+  useEffect(() => {
     const fetchTotalQuantity = async () => {
       const response = await fetch(`${baseUrl}/api/payments-total-quantity?blogId=${_id}`);
       const data = await response.json();
-      const status = (quantity-data) > 0 ? "Stock In" : "Stock Out";
+      const status = (quantity - data) > 0 ? "Stock In" : "Stock Out";
       setStock(status);
     };
     fetchTotalQuantity();
   }, [baseUrl, _id]);
-
-
-  // console.log(cardCount);
-
 
   const addToBookmark = async () => {
     if (isBookmarked) {
@@ -48,32 +39,18 @@ useEffect(() => {
     }
 
     try {
-      // const response = await axios.post(`/api/wishlist/${_id}`, {
-      //   name,
-      //   description: book.description || "",
-      //   image,
-      //   author: book.author || "",
-      //   price,
-      //   rating: ratings,
-      //   category,
-      // });
-
-      const response = await axios.post(
-        "/api/wishlists",
-        {
-          name,
-          description: book.description || "",
-          image,
-          author: book.author || "",
-          price,
-          rating: ratings,
-          category,
-          quantity,
-          email: session?.user?.email,
-          cardCount,
-        },
-        { email: session?.user?.email } // Pass email in the request body
-      );
+      const response = await axios.post("/api/wishlists", {
+        name,
+        description: book.description || "",
+        image,
+        author: book.author || "",
+        price,
+        rating: ratings,
+        category,
+        quantity,
+        email: session?.user?.email,
+        cardCount,
+      });
       console.log(response);
 
       if (response.status === 201) {
@@ -88,8 +65,7 @@ useEffect(() => {
       }
     } catch (error) {
       console.error("Error adding to bookmark:", error);
-      const message =
-        error.response?.data?.message || "Failed to add to bookmarks!";
+      const message = error.response?.data?.message || "Failed to add to bookmarks!";
 
       if (error.response?.status === 409) {
         Swal.fire({
@@ -118,22 +94,18 @@ useEffect(() => {
     }
 
     try {
-      const response = await axios.post(
-        "/api/carts",
-        {
-          name,
-          description: book.description || "",
-          image,
-          author: book.author || "",
-          price,
-          rating: ratings,
-          category,
-          quantity,
-          email: session?.user?.email,
-          cardCount,
-        },
-        { email: session?.user?.email } // Pass email in the request body
-      );
+      const response = await axios.post("/api/carts", {
+        name,
+        description: book.description || "",
+        image,
+        author: book.author || "",
+        price,
+        rating: ratings,
+        category,
+        quantity,
+        email: session?.user?.email,
+        cardCount,
+      });
 
       if (response.status === 201) {
         setIsInCart(true);
@@ -168,8 +140,7 @@ useEffect(() => {
   return (
     <div className="transition h-fit duration-500 w-full font-sans overflow-hidden mx-auto mt-4 pl-4 pt-4">
       {/* Full Height Image */}
-      
-      <div className="w-full  h-60 md:h-80 lg:h-52 relative group">
+      <div className="w-full h-60 md:h-80 lg:h-52 relative group">
         <Image
           src={image}
           alt={name}
@@ -181,23 +152,17 @@ useEffect(() => {
         <div>
           <button
             onClick={addToBookmark}
-            className={`cursor-pointer absolute bottom-16 right-2 p-2 rounded-full bg-white shadow-md transition-transform duration-500 opacity-0 group-hover:opacity-100 hover:duration-500 ${
-              isBookmarked ? "text-[#F65D4E]" : ""
-            }`}
+            className={`cursor-pointer absolute bottom-16 right-2 p-2 rounded-full bg-white shadow-md transition-transform duration-500 opacity-0 group-hover:opacity-100 hover:duration-500 ${isBookmarked ? "text-[#F65D4E]" : ""}`}
           >
             <FaHeart className="text-xl" />
           </button>
           <button
             onClick={addToCart}
-            className={`cursor-pointer absolute bottom-5 right-2 p-2 rounded-full bg-white shadow-md transition-transform duration-500 opacity-0 group-hover:opacity-100 hover:duration-500 ${
-              isInCart ? "text-[#F65D4E]" : ""
-            }`}
+            className={`cursor-pointer absolute bottom-5 right-2 p-2 rounded-full bg-white shadow-md transition-transform duration-500 opacity-0 group-hover:opacity-100 hover:duration-500 ${isInCart ? "text-[#F65D4E]" : ""}`}
           >
             <FaShoppingCart className="text-xl" />
           </button>
-
         </div>
-
       </div>
 
       {/* Book Details */}
@@ -225,11 +190,9 @@ useEffect(() => {
           </span>
         </h3>
         <h2 className="flex gap-2"><span>{stock}</span>
-
-        <span>{publishType === "released" ? "" : "upcoming"}</span>
+          <span>{publishType === "released" ? "" : "upcoming"}</span>
         </h2>
       </div>
-
     </div>
   );
 }
