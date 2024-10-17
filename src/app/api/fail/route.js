6c9eb72add423da8d623256a26df780aaaea186d
Mohammad Azad <61772814+azaaaaaaaaad@users.noreply.github.com>
2024-10-17 12:05:37 +0600

    Delete src/app/api/fail directory

--- src/app/api/fail/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-export async function POST(req, res) {
-  // database operations
-  // b2b 3rd party secure call
-  // fail log
-  return NextResponse.redirect(new URL("/fail", req.url), 303);
-}
