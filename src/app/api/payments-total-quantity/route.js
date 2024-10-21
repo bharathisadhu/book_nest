484f1ddeacc0f9b014864b00c6c6cc6b801f94ce
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-10-21 23:56:05 +0600

    check stock and pre-order

--- src/app/api/payments-total-quantity/route.js
+++ src/app/api/payments-total-quantity/route.js
@@ -80,7 +80,7 @@ export async function GET(req) {
     const url = new URL(req.url);
     const bookId = url.searchParams.get('blogId');
 
-     // Calculate the total quantity for the specified bookId
+     // Calculate the total quantity for the specified 
       let totalQuantity = 0;
       data.forEach(order => {
         order.books.forEach(book => {
