9bfcc6fa768a30c4ac2db36b830f57969e01fd1b
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-10-23 01:33:46 +0600

    payment by user email pagination

--- src/app/api/payments-pagination/[email]/route.js
+++ src/app/api/payments-pagination/[email]/route.js
@@ -1,25 +1,35 @@
-import connectToDatabase from "@/lib/mongodb";
-import { WishList } from "../../../../../models/Book";
 import { NextResponse } from "next/server";
+import { ObjectId } from "mongodb";
+import { Payment } from "../../../../../models/Payment";
+import connectToDatabase from "@/lib/mongodb";
 
+let db;
+
+// Handle POST requests
+// Handle POST requests
+
+
+// Handle GET requests
 export async function GET(request, { params }) {
   const { email } = params;
 
   await connectToDatabase();
 
   try {
+
+
     const { searchParams } = new URL(request.url);
     const page = parseInt(searchParams.get("page") || "1", 10);
     const limit = parseInt(searchParams.get("limit") || "10", 10);
     const skip = (page - 1) * limit;
 
-    const wishlists = await WishList.find({ email: email })
+    const Payment = await Payment.find({ email: email })
       .skip(skip)
       .limit(limit);
       
-      const totalWishList = await WishList.countDocuments({ email: email });
+      const totalPayment = await Payment.countDocuments({ email: email });
 
-    if (!wishlists) {
+    if (!Payment) {
       return NextResponse.json(
         { message: " individualCart not found" },
         { status: 404 }
@@ -28,11 +38,11 @@ export async function GET(request, { params }) {
 
     return new Response(JSON.stringify({
       success: true,
-      data:wishlists,
+      data:Carts,
       email: email,
-      total: totalWishList,
+      total: totalPayment,
       page,
-      totalPages: Math.ceil(totalWishList / limit),
+      totalPages: Math.ceil(totalPayment / limit),
       
     }), {
       status: 200,
@@ -40,9 +50,14 @@ export async function GET(request, { params }) {
 
 
 
-    // return NextResponse.json(individualWishList, { status: 200 });
 
 
+
+    
+
+
+    // console.log(individualCart);
+    return NextResponse.json(individualCart, { status: 200 });
   } catch (error) {
     console.error("Error fetching user:", error);
     return NextResponse.json({ message: "Server error" }, { status: 500 });
