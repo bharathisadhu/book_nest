57f14c7d26424ce86c192991c755a441ebaf015f
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-10-22 21:08:16 +0600

    user wishlist pagination

--- src/app/api/wishlists/[email]/route.js
+++ src/app/api/wishlists/[email]/route.js
@@ -8,14 +8,41 @@ export async function GET(request, { params }) {
   await connectToDatabase();
 
   try {
-    const individualWishList = await WishList.find({ email: email });
-    if (!individualWishList) {
+    const { searchParams } = new URL(request.url);
+    const page = parseInt(searchParams.get("page") || "1", 10);
+    const limit = parseInt(searchParams.get("limit") || "10", 10);
+    const skip = (page - 1) * limit;
+
+    const wishlists = await WishList.find({ email: email })
+       .skip(skip)
+      .limit(limit);
+      
+      const totalWishList = await WishList.countDocuments({ email: email });
+
+    if (!wishlists) {
       return NextResponse.json(
         { message: " individualCart not found" },
         { status: 404 }
       );
     }
-    return NextResponse.json(individualWishList, { status: 200 });
+
+    return new Response(JSON.stringify({
+      success: true,
+      data:wishlists,
+      email: email,
+      total: totalWishList,
+      page,
+      totalPages: Math.ceil(totalWishList / limit),
+      
+    }), {
+      status: 200,
+    });
+
+
+
+    // return NextResponse.json(individualWishList, { status: 200 });
+
+
   } catch (error) {
     console.error("Error fetching user:", error);
     return NextResponse.json({ message: "Server error" }, { status: 500 });
