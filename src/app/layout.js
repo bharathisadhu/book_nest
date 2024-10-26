cb41a20001b17a2d6f69fd57f3e4b71ac64fd4a7
monsur28 <monsur286512@gmail.com>
2024-10-26 19:04:50 +0600

    Socket Io implemented

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,66 @@
+"use client";
 import localFont from "next/font/local";
 import "./globals.css";
 import AuthProvider from "@/services/AuthProvider";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { MdSupportAgent } from "react-icons/md";
+import { useEffect, useState } from "react";
+import Chat from "@/components/chat";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-export const metadata = {
+const metadata = {
   title: "BookNest",
   description: "Read for Peace",
   favicon: "BookNest.png",
 };
 
 export default function RootLayout({ children }) {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+
+  // Toggle body overflow and modal state
+  const toggleChat = () => {
+    setIsChatOpen((prev) => !prev);
+  };
+
+  // Apply the "modalOpen" class to <body> when the modal is open
+  useEffect(() => {
+    if (isChatOpen) {
+      document.body.classList.add("modalOpen");
+    } else {
+      document.body.classList.remove("modalOpen");
+    }
+  }, [isChatOpen]);
+
   return (
     <html lang="en" data-theme="cupcake">
       <body className="font-poppins">
         <AuthProvider>
-          <div>{children}</div>
+          <div>
+            <Navbar />
+            {children}
+            <Footer />
+            <button
+              onClick={toggleChat}
+              className="fixed bottom-6 right-6 p-4 bg-blue-500 text-white rounded-full shadow-lg z-30 hover:bg-blue-600 transition duration-300"
+              aria-label="Support"
+            >
+              <MdSupportAgent size={40} />
+            </button>
+            {/* Chat Modal */}
+            {isChatOpen && (
+              <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+                <div className="bg-white p-6 rounded-lg max-w-md w-full relative">
+                  <button
+                    onClick={toggleChat}
+                    className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+                  >
+                    ✕
+                  </button>
+                  <Chat />
+                </div>
+              </div>
+            )}
+          </div>
         </AuthProvider>
       </body>
     </html>
