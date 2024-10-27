b3748d7b45341739f5f53a7359d4577f2a79cc9f
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-10-28 02:09:40 +0600

    privateRoute added on the dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -43,16 +43,6 @@ const Users = () => {
         <title>BookNest | Dashboard</title>
       </Head>
       <DashboardLayout>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-          <UserCount className=" col-span-1" />
-          <BookCount className=" col-span-1" />
-          <SaleCount className=" col-span-1" />
-          <BlogCount className=" col-span-1" />
-        </div>
-        <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5 text-black">
-          <BarCharts totalPrice={100000} /> {/* Pass totalPrice as needed */}
-          <ProfitChart />
-        </div>
       </DashboardLayout>
     </>
   );
