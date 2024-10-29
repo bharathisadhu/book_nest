e517303e28a6c5605db55f18766f236c4a3d96dd
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-10-29 11:00:35 +0600

    invoice api via check post man

---
+++ src/app/api/invoice/[id]/route.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/lib/connectDB";
+import { ObjectId } from "mongodb";
+
+let db;
+
+export async function GET(request, { params }) {
+  const { id } = params;
+
+  db = await connectDB();
+
+  const payments = await db.collection("payments").findOne({ _id: new ObjectId(id) }); // Convert id to ObjectId
+  if (!payments) {
+    return NextResponse.json({ message: "Book not found" }, { status: 404 });
+  }
+
+  return NextResponse.json(payments);
+}
+
+
