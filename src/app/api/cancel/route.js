fea84800120f7092bf5882ce0188342758c1e259
Bharathi Saadu <bharathisadhu139@gmail.com>
2025-06-30 12:34:36 +0530

    Update route.js

--- src/app/api/cancel/route.js
+++ src/app/api/cancel/route.js
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 export async function POST(req, res) {
   // database operations
   // b2b 3rd party secure call
-  // cancel log
+  // cancel log 
   return NextResponse.redirect(new URL("/cancel", req.url), 303);
 }
