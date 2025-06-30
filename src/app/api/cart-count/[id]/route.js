cf226765a6837d74e0190acc4177ba26c490f05c
Bharathi Saadu <bharathisadhu139@gmail.com>
2025-06-30 12:35:04 +0530

    Update route.js

--- src/app/api/cart-count/[id]/route.js
+++ src/app/api/cart-count/[id]/route.js
@@ -1,6 +1,6 @@
 // import { NextResponse } from 'next/server';
 // import { connectToDatabase } from '@/lib/mongodb';
-
+ 
 // export async function POST(request, { params }) {
 //   const { id } = params;
 //   const bookData = await request.json();
