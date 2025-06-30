c33e57690674df4677c4c03b8be1ba758f54f271
Bharathi Saadu <bharathisadhu139@gmail.com>
2025-06-30 12:20:06 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 // models/User.js
 import mongoose from "mongoose";
-
+ 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   stripeSessionId: String,
