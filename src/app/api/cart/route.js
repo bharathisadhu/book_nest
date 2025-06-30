b5776120e169f858fc3e56d999e30ed839176008
Bharathi Saadu <bharathisadhu139@gmail.com>
2025-06-30 12:36:26 +0530

    Update route.js

--- src/app/api/cart/route.js
+++ src/app/api/cart/route.js
@@ -1,6 +1,6 @@
 // import { NextResponse } from 'next/server';
 // import { connectToDatabase } from '@/lib/mongodb';
-
+ 
 // export async function GET() {
 //   try {
 //     const client = await connectToDatabase();
