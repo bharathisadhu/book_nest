76cbb15bc23f0020507d1cc4c12918459087e853
Bharathi Saadu <bharathisadhu139@gmail.com>
2025-06-30 12:38:53 +0530

    Update route.js

--- src/app/api/fail/route.js
+++ src/app/api/fail/route.js
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 export async function POST(req, res) {
   // database operations
   // b2b 3rd party secure call
-  // fail log
+  // fail log 
   return NextResponse.redirect(new URL("/fail", req.url), 303);
 }
