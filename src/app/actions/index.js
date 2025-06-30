f312157ff4791f7643171aa0c11c2af95d8ea03e
Bharathi Saadu <bharathisadhu139@gmail.com>
2025-06-30 12:27:01 +0530

    Update index.js

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,5 +1,5 @@
 "use server";
-
+ 
 export async function doSocialLogin(action) {
   // The logic for this function will now be moved to the client
   console.log(action); // If you need this on the server, move the logging logic here
